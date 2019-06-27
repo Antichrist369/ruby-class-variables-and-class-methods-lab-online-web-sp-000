caa7873fc805917322eeef479bfe57a1e1de8eb4
Anthony Molina <antonio-molina@att.net>
2019-06-27 05:39:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,35 @@ class Song
     @@artists = [] 
     
     def initialize(song_name, artist, genre) 
-      @@count +=1
+      @@name = song_name 
+      @@artist = artist 
+      @@genre = genre 
+      @@count += 1
+      @@genres << genre 
+      @@artists << artist 
+    end 
+    
+    def self.count 
+      @@count 
+    end 
+    
+    def self.genres 
+      @@genres.uniq! 
+    end 
+    
+    def .artists 
+      @@artists.uniq!
+    end 
+    
+    def self.genre_count 
+      genre_count = {} 
+      @@genres.each do |genre| 
+        if genre_count[genre] 
+          genre_count[genre] += 1 
+        else 
+          genre_count[genre] = 1 
+        end 
+      end 
+      genre_count
   
 end 
